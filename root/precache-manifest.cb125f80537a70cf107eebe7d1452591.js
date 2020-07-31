c606f13514784268896caf98f9d03e5de3c8bce0
Hugo <hugoandres272@gmail.com>
2020-07-31 14:17:07 -0500

    Cambiando root

---
+++ root/precache-manifest.cb125f80537a70cf107eebe7d1452591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c581ebc5bf2d77d199a149804f413b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd8911cd9eae0c9d996",
+    "url": "/static/css/main.b0b1ef0e.chunk.css"
+  },
+  {
+    "revision": "26131dc4c49e0825b591",
+    "url": "/static/js/2.4a950b34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a950b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd8911cd9eae0c9d996",
+    "url": "/static/js/main.d4a55a2e.chunk.js"
+  },
+  {
+    "revision": "5ca5ac6fe14f7f337f5a",
+    "url": "/static/js/runtime-main.5f98d68a.js"
+  }
+]);
